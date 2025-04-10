4b6c0ee7ee0ea8aadefe46f0859973e1d46befb1
chafiq1992 <chfiqchoqi@gmail.com>
2025-04-10 20:29:47 +0100

    Initial commit

---
+++ backend/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import boto3
+import os
+
+app = FastAPI()
+
+@app.get("/r2-images")
+def list_r2_images():
+    access_key = os.getenv("R2_ACCESS_KEY")
+    secret_key = os.getenv("R2_SECRET_KEY")
+    endpoint = os.getenv("R2_ENDPOINT")
+    bucket = os.getenv("R2_BUCKET")
+
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=endpoint,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+
+    image_urls = []
+    response = s3.list_objects_v2(Bucket=bucket)
+    for obj in response.get("Contents", []):
+        key = obj["Key"]
+        if key.lower().endswith((".jpg", ".jpeg", ".png")):
+            url = f"{endpoint}/{bucket}/{key}"
+            image_urls.append(url)
+
+    return image_urls
