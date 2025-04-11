4cf1b46cf6aea695d16c4412fded659ed5703aac
chafiq1992 <79022031+chafiq1992@users.noreply.github.com>
2025-04-11 13:32:45 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Gallery from './Gallery.jsx';
+import App from './App'
+import './index.css' // ✅ Add this line to load Tailwind
 import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
